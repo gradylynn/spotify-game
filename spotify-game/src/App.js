0e8b5998a10108a57652c4a33f3679bf2c3cb9a4
Grady Lynn <gradylynn314@gmail.com>
2023-10-07 09:46:07 -0500

    initial TrackChoice component

--- spotify-game/src/App.js
+++ spotify-game/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello world!</p>
+        <TrackChoice trackId='0KhKVGSHBhUs8hIexyvwj4'></TrackChoice>
+      </div>
+    );
+  }
+}
+
+class TrackChoice extends Component {
+  render() {
+    return (
+      <div>
+        <iframe
+          style={{"border-radius": "12px"}}
+          src={`https://open.spotify.com/embed/track/${this.props.trackId}`}
+          width="100%"
+          height="352"
+          frameBorder="0"
+          allowfullscreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      </div>
+    );
+  }
 }
 
 export default App;
