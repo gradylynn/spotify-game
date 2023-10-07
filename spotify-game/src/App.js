cdbf4918e17b42f2051dab07efb2ec45c030b74d
Grady Lynn <gradylynn314@gmail.com>
2023-10-07 10:55:21 -0500

    material card nonsense

--- spotify-game/src/App.js
+++ spotify-game/src/App.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 class App extends Component {
   render() {
     return (
       <div>
         <p>Hello world!</p>
-        <TrackChoice trackId='0KhKVGSHBhUs8hIexyvwj4'></TrackChoice>
+        <Container>
+          <Card
+            raised={true}
+          >
+            <Grid container alignItems="center" justifyContent="center">
+              <Grid xs={10}>
+                <Box sx={{ my: 5, ml: 5 }}>
+                  <TrackChoice trackId='27r42A2NMzg2hoFZjQFrm3'></TrackChoice>
+                </Box>
+              </Grid>
+              <Grid container xs={2} alignItems="center" justifyContent="center">
+                <Box sx={{ m: 5 }}>
+                  <Checkbox {...label} size="large"></Checkbox>
+                </Box>
+              </Grid>
+            </Grid>
+          </Card>
+        </Container>
       </div>
     );
   }
@@ -16,6 +51,7 @@ class TrackChoice extends Component {
     return (
       <div>
         <iframe
+          title='potato'
           style={{"border-radius": "12px"}}
           src={`https://open.spotify.com/embed/track/${this.props.trackId}`}
           width="100%"
