d3bd5ff954f947bc31046f49fdccf5647014933d
Grady Lynn <gradylynn314@gmail.com>
2023-10-12 05:56:29 -0400

    color and moving towards state

--- spotify-game/src/App.js
+++ spotify-game/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -12,86 +13,92 @@ import { useTheme } from '@mui/material/styles';
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 const App = () => {
-    return (
-      <div>
-        <Container>
-          <Grid container sx={{height: '100vh'}} direction="column" justifyContent="space-around" wrap="nowrap">
-            <Grid item xs={1}>
-              <Typography variant="h2">
-                Spotter
-              </Typography>
-            </Grid>
-            <Grid item xs={1}>
-              <Typography variant="h4">
-                Predict which song will have more total listens in 2 weeks:
-              </Typography>
-            </Grid>
-            <Grid item xs={1}>
-              <TrackCard trackId='5LAUpU2KhoVDnur463CAuT'></TrackCard>
-            </Grid>
-            <Grid item xs={1}>
-              <TrackCard trackId='4JyZnltqvgBqTRLCMxj6Kk'></TrackCard>
-            </Grid>
-            <Grid item xs={1} alignSelf="center">
-              <Button variant="contained">Submit</Button>
-            </Grid>
+  const [selection, setSelection] = useState(0);
+
+  return (
+    <div backgroundColor='blue'>
+      <Container>
+        <Grid container spacing={2} sx={{height: '100vh'}} direction="column" justifyContent="space-around" wrap="nowrap">
+          <Grid item xs={1}>
+            <Typography variant="h2">
+              Spotter
+            </Typography>
           </Grid>
-        </Container>
-      </div>
-    );
-  }
-
-
-const TrackCard = ({trackId}) => {
-      const cardHeights = {
-        xs: 120,
-        sm: 120,
-        md: 210,
-        lg: 210,
-        xl: 300,
-      }
-
-      return <Card raised={true} sx={{height: cardHeights}}>
-        <Grid container sx={{height: cardHeights}} alignItems="center">
-          <Grid item xs={10}>
-            <Box sx={{ml: {xs: 20/8, sm: 20/8, md: 29/8, lg: 29/8, xl: 34/8}, mr: 0}}>
-              <TrackChoice trackId={trackId}></TrackChoice>
-            </Box>
+          <Grid item xs={1}>
+            <Typography variant="h4">
+              Predict which song will have more total listens in 2 weeks:
+            </Typography>
           </Grid>
-          <Grid item xs={2}>
-            <Grid container justifyContent="center">
-              <Grid item>
-                <Checkbox {...label} size="large"></Checkbox>
-              </Grid>
+          <Grid item xs={1}>
+            <TrackCard trackId='5LAUpU2KhoVDnur463CAuT' isSelected={true}></TrackCard>
+          </Grid>
+          <Grid item xs={1}>
+            <TrackCard trackId='4JyZnltqvgBqTRLCMxj6Kk' isSelected={false}></TrackCard>
+          </Grid>
+          <Grid item xs={1} alignSelf="center">
+            <Button variant="contained">Submit</Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+
+const TrackCard = ({trackId, isSelected}) => {
+    const cardHeights = {
+      xs: 120,
+      sm: 120,
+      md: 210,
+      lg: 210,
+      xl: 300,
+    }
+
+    // const [selected, setSelected] = useState(true);
+
+    return <Card raised={isSelected} sx={{height: cardHeights, backgroundColor: isSelected ? '#DCFEFD': '#FFFFFF'}}>
+      <Grid container sx={{height: cardHeights}} alignItems="center">
+        <Grid item xs={10}>
+          <Box sx={{ml: {xs: 20/8, sm: 20/8, md: 29/8, lg: 29/8, xl: 34/8}, mr: 0}}>
+            <TrackChoice trackId={trackId}></TrackChoice>
+          </Box>
+        </Grid>
+        <Grid item xs={2}>
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Checkbox {...label} size="large"></Checkbox>
             </Grid>
           </Grid>
         </Grid>
-    </Card>
-  }
+      </Grid>
+  </Card>
+}
 
 const TrackChoice = ({trackId}) => {
-    const theme = useTheme();
-    let iframeHeight = 80;
-    if (useMediaQuery(theme.breakpoints.up('md'))) {
-      iframeHeight += 72;
-    }
-    if (useMediaQuery(theme.breakpoints.up('xl'))) {
-      iframeHeight += 80;
-    }
-
-    return (
-      <iframe
-        title='spotify-track'
-        style={{"border-radius": "12px"}}
-        src={`https://open.spotify.com/embed/track/${trackId}`}
-        width="100%"
-        height={iframeHeight}
-        frameBorder="0"
-        allowfullscreen=""
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      ></iframe>
-    );
+  const theme = useTheme();
+  let iframeHeight = 80;
+  if (useMediaQuery(theme.breakpoints.up('md'))) {
+    iframeHeight += 72;
   }
+  if (useMediaQuery(theme.breakpoints.up('xl'))) {
+    iframeHeight += 80;
+  }
+
+  return (
+    <iframe
+      title='spotify-track'
+      style={{"border-radius": "12px"}}
+      src={`https://open.spotify.com/embed/track/${trackId}`}
+      width="100%"
+      height={iframeHeight}
+      frameBorder="0"
+      allowfullscreen=""
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    ></iframe>
+  );
+}
+
+document.body.style = 'background: #FFFEC8;';
 
 export default App;
