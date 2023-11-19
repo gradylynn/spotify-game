9afe77ff87bcdf0359d59afcc024cbfe05c6c54b
Grady Lynn <gradylynn314@gmail.com>
2023-11-18 20:41:37 -0500

    some python organization

---
+++ data/get_track_data.py
@@ -0,0 +1,50 @@
+import os
+import time
+import json
+from datetime import datetime, timedelta
+import requests
+import pandas as pd
+
+this_dir = os.path.dirname(os.path.realpath(__file__))
+
+token = requests.post(
+    'https://accounts.spotify.com/api/token',
+    data={'grant_type': 'client_credentials'},
+    auth=requests.auth.HTTPBasicAuth(
+        os.environ['SPOTIFY_CLIENT_ID'],
+        os.environ['SPOTIFY_CLIENT_SECRET']
+    )
+).json()
+
+def get_track_info(track_id):
+    track_resp = requests.get(
+        f'https://api.spotify.com/v1/tracks/{track_id}',
+        headers={'Authorization': f'Bearer {token["access_token"]}'}
+    )
+    time.sleep(.5)  # poor man's rate limiter
+    playcount_resp = requests.get(
+        f'https://api.t4ils.dev/albumPlayCount',
+        params={'albumid': track_resp.json()['album']['id']}
+    )
+    for album_track in sum((d['tracks'] for d in playcount_resp.json()['data']['discs']), start=[]):
+        if track_id in album_track['uri']:
+            return album_track
+
+
+def main():
+    # planning to run this ~12am eastern
+    tomorrow = datetime.date(datetime.utcnow())
+
+    tracks_df = pd.DataFrame(json.load(open(os.path.join(this_dir, 'tracks.json'), 'r')))
+    schedule_df = pd.DataFrame(json.load(open(os.path.join(this_dir, 'schedule.json'), 'r')))
+
+    window_df = schedule_df[(schedule_df['date'] <= str(tomorrow)) & (schedule_df['date'] >= str(tomorrow - timedelta(days=32)))]
+    window_df = window_df.merge(tracks_df, how='left', on=['date', 'track1Id', 'track2Id'])
+    window_df['track1Playcount'] = window_df.apply(lambda x: x['track1Playcount'] if pd.notnull(x['track1Playcount']) else get_track_info(x['track1Id'])['playcount'], axis=1)
+    window_df['track2Playcount'] = window_df.apply(lambda x: x['track2Playcount'] if pd.notnull(x['track2Playcount']) else get_track_info(x['track2Id'])['playcount'], axis=1)
+    
+    with open(os.path.join(this_dir, 'tracks.json'), 'w') as f:
+        json.dump(window_df.sort_values('date', ascending=False).to_dict('records'), f)
+
+if __name__ == '__main__':
+    main()
--- src/utilities.js
+++ src/utilities.js
@@ -23,7 +23,7 @@ const MONTHS = [
 ];
 
 const NOW = new Date();
-NOW.setHours(NOW.getHours() + 8); // date cutoff will be 8 hrs after UTC midnight
+NOW.setHours(NOW.getHours() + 5); // date cutoff will be 5 hrs after UTC midnight
 const TODAY = new Date(
     `${NOW.getUTCFullYear()}-${String(NOW.getUTCMonth()).padStart(2, '0')}-${String(NOW.getUTCDate()).padStart(2, '0')}T00:00:00`
 );
@@ -33,7 +33,7 @@ YESTERDAY.setDate(YESTERDAY.getDate() - 1);
 // NOTE: this block here is just for testing
 if (!Cookies.get('selections')) {
     Cookies.set('selections', '012201211');
-    Cookies.set('lastSelectionDate', (new Date('1970-01-24T00:00:00')).getTime());
+    Cookies.set('lastSelectionDate', (new Date('2023-11-16T00:00:00')).getTime());
 }
 
 // return today's selection using cookie data
